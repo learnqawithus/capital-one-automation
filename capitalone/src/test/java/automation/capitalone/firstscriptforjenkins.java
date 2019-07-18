fc95cd955b766913c8aa37df141d0e519b324b69
Fawad Anwar <Fawad Anwar@10.0.0.76>
2019-07-17 22:17:06 -0500

    first commit

---
+++ capitalone/src/test/java/automation/capitalone/firstscriptforjenkins.java
@@ -0,0 +1,13 @@
+package automation.capitalone;
+
+import org.testng.annotations.Test;
+
+public class firstscriptforjenkins {
+	
+	@Test
+	public void printline() {
+		
+		System.out.println("I am awesome!!!!");
+	}
+
+}
